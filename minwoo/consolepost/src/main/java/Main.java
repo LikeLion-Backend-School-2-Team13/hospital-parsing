c2c23fe42ae3f2b16f0ddb26152f74833ec41974
menuhwang <hmin0206@naver.com>
2022-10-16 13:02:16 +0900

    ConsolePostService 비즈니스 계층 추가

--- minwoo/consolepost/src/main/java/Main.java
+++ minwoo/consolepost/src/main/java/Main.java
@@ -1,11 +1,13 @@
 import controller.ConsolePostController;
 import repository.ConsolePostRepository;
+import service.ConsolePostService;
 import view.ConsolePostView;
 
 public class Main {
     public static void main(String[] args) {
         ConsolePostRepository repository = new ConsolePostRepository();
-        ConsolePostController controller = new ConsolePostController(repository);
+        ConsolePostService service = new ConsolePostService(repository);
+        ConsolePostController controller = new ConsolePostController(service);
         ConsolePostView view = new ConsolePostView();
         ConsolePostApplication consolePostApplication = new ConsolePostApplication(view, controller);
 
